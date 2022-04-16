497d108a4c28e916d175602fd37749bc754e43fc
Piash <piash.hassan@bjitgroup.com>
2022-04-16 18:59:17 +0600

    - Update ktor 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 plugins {
     application
-    kotlin("jvm") version "1.6.0"
+    kotlin("jvm") version "1.6.20"
 }
 
 group = "com.example"
@@ -45,26 +45,26 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 
     // exposed ORM library
-    implementation("org.flywaydb:flyway-core:8.4.1")
-    implementation("org.jetbrains.exposed:exposed-core:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-dao:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-java-time:0.37.3")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.37.3")
+    implementation("org.flywaydb:flyway-core:8.5.7")
+    implementation("org.jetbrains.exposed:exposed-core:0.38.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.38.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.38.1")
+    implementation("org.jetbrains.exposed:exposed-java-time:0.38.1")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.38.1")
 
     // postgresql
-    implementation("org.postgresql:postgresql:42.3.1")
+    implementation("org.postgresql:postgresql:42.3.3")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
     // password hashing
     implementation("at.favre.lib:bcrypt:0.9.0")
     // date time
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
     // mail server
     implementation("org.apache.commons:commons-email:1.5")
 
-    implementation("com.google.api-client:google-api-client:1.33.0")
-    implementation("com.google.oauth-client:google-oauth-client:1.32.1")
+    implementation("com.google.api-client:google-api-client:1.33.4")
+    implementation("com.google.oauth-client:google-oauth-client:1.33.2")
 
     // validator
     implementation("org.valiktor:valiktor-core:0.12.0")
