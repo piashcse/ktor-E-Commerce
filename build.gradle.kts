54e585b4a8e1d074df6ab3d1a7d3859a7f26b604
mehedihasanpiash <piash599@gmail.com>
2024-04-07 20:28:50 +0600

    - Update ktor, exposed, kotlin, gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,11 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.9.21"
-    id("com.github.johnrengelman.shadow") version "8.0.0"
+    kotlin("jvm") version "1.9.23"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "com.example"
+group = "com.piashcse"
 version = "0.0.1"
 application {
      mainClass.set("com.example.ApplicationKt")
@@ -44,7 +44,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
 
     // exposed ORM library
-    implementation("org.flywaydb:flyway-core:9.16.0")
+    implementation("org.flywaydb:flyway-core:9.22.3")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
@@ -52,13 +52,13 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
 
     // postgresql
-    implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.postgresql:postgresql:42.7.3")
     implementation("com.zaxxer:HikariCP:5.0.1")
 
     // password hashing
     implementation("at.favre.lib:bcrypt:0.10.2")
     // date time
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
     // mail server
     implementation("org.apache.commons:commons-email:1.5")
     // validator
