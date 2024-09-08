be98b1aa9093cf0651f72abc8d01bb72c53fdd1b
mehedihasanpiash <piash599@gmail.com>
2024-09-08 19:19:12 +0600

    - Added version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,15 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val exposed_version: String by project
-val logback_version: String by project
-
 plugins {
-    application
-    kotlin("jvm") version "2.0.20"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.shadow)
 }
 
 group = "com.piashcse"
 version = "1.0.0"
 application {
-     mainClass.set("com.piashcse.ApplicationKt")
-     project.setProperty("mainClassName", "com.piashcse.ApplicationKt") // adding this for fatjar
+    mainClass.set("com.piashcse.ApplicationKt")
+    project.setProperty("mainClassName", "com.piashcse.ApplicationKt") // adding this for fatjar
 
 }
 
@@ -24,52 +20,41 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-server-host-common:$ktor_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-request-validation:$ktor_version")
-    // authentication
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-gson:$ktor_version")
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.ktor.serialization.gson)
+    implementation(libs.ktor.server.host.common)
+    implementation(libs.ktor.server.status.pages)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.call.logging)
+    implementation(libs.ktor.server.request.validation)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
 
-    implementation("io.ktor:ktor-server-cors:$ktor_version")
-    implementation("io.ktor:ktor-server-compression:$ktor_version")
-    implementation("io.ktor:ktor-server-status-pages:$ktor_version")
-    implementation("io.ktor:ktor-server-freemarker:$ktor_version")
-    implementation("io.ktor:ktor-server-call-logging:$ktor_version")
+    implementation(libs.flyway.core)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.java.time)
+    implementation(libs.exposed.kotlin.datetime)
 
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+    implementation(libs.postgresql)
+    implementation(libs.hikari)
 
-    // exposed ORM library
-    implementation("org.flywaydb:flyway-core:9.22.3")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposed_version")
+    implementation(libs.bcrypt)
+    implementation(libs.kotlinx.datetime)
+    implementation(libs.commons.email)
+    implementation(libs.valiktor.core)
+    implementation(libs.commons.io)
 
-    // postgresql
-    implementation("org.postgresql:postgresql:42.7.4")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-
-    // password hashing
-    implementation("at.favre.lib:bcrypt:0.10.2")
-    // date time
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-    // mail server
-    implementation("org.apache.commons:commons-email:1.5")
-    // validator
-    implementation("org.valiktor:valiktor-core:0.12.0")
-    // file extension
-    implementation("commons-io:commons-io:2.11.0")
-    //swagger
-    implementation("io.github.smiley4:ktor-swagger-ui:3.3.1")
-    implementation("io.swagger.parser.v3:swagger-parser:2.1.22")
-
+    implementation(libs.ktor.swagger.ui)
+    implementation(libs.swagger.parser)
 }
 tasks.create("stage") {
     dependsOn("installDist")
