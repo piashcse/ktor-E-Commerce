d090d8bc722dac56e575b619fe27c87f7dcb9e40
Mehedi Hassan Piash <piash599@gmail.com>
2025-09-19 12:47:20 +0600

    - Code optimization

--- src/main/kotlin/com/piashcse/utils/DotEnv.kt
+++ src/main/kotlin/com/piashcse/utils/DotEnv.kt
@@ -1,27 +1,21 @@
 package com.piashcse.utils
 
+import io.github.cdimascio.dotenv.Dotenv
 import io.github.cdimascio.dotenv.dotenv
 
 object DotEnv {
-    private val dotenv = dotenv {
+    private val dotenv: Dotenv = dotenv {
         directory = "./"
         filename = ".env"
         ignoreIfMissing = true
+        ignoreIfMalformed = true
     }
 
-    fun get(key: String): String? {
-        return dotenv[key]
-    }
+    fun get(key: String): String? = dotenv[key]
 
-    fun get(key: String, defaultValue: String): String {
-        return dotenv[key] ?: defaultValue
-    }
+    fun get(key: String, defaultValue: String): String = dotenv[key] ?: defaultValue
 
-    fun getInt(key: String, defaultValue: Int): Int {
-        return dotenv[key]?.toIntOrNull() ?: defaultValue
-    }
+    fun getInt(key: String, defaultValue: Int): Int = dotenv[key]?.toIntOrNull() ?: defaultValue
 
-    fun getBoolean(key: String, defaultValue: Boolean): Boolean {
-        return dotenv[key]?.toBoolean() ?: defaultValue
-    }
+    fun getBoolean(key: String, defaultValue: Boolean): Boolean = dotenv[key]?.toBoolean() ?: defaultValue
 }
